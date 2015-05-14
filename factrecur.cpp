501879d339c694a187af6cb7a0515f8120035796
ramrnitt <rahulramaswamy97@gmail.com>
2015-05-14 14:22:51 +0530

    recursion.cpp
    a program to calculate factorial using recursion

---
+++ factrecur.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+
+long int fact(int v)
+{
+  if(v==0)
+    return 1;
+  else
+    return(v*fact(v-1));
+  }
+void main()
+{
+  int var;
+  cout<<"enter to calculate factorial:";
+  cin>>var;
+  fact(var);
+  getch();
+  }
